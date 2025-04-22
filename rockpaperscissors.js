2764d0544efd9bab42ac532e65c98dc012a82590
NiTzuA <11v35faustin@gmail.com>
2025-04-22 13:39:28 +0800

    Added a UI

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,5 +1,5 @@
 let getComputerChoice = () => {
-    random = Math.floor(Math.random() * 3)
+    let random = Math.floor(Math.random() * 3)
     switch (random) {
         case 0:
             return "rock";
@@ -12,36 +12,78 @@ let getComputerChoice = () => {
     };
 }
 
-let getHumanChoice = () => {
-    let userInput = prompt("Welcome to Rock, Paper, and Scissors! \n\nType either Rock, Paper, or Scissors!");
-    return userInput.toLowerCase();
-}
-
 humanScore = 0;
 computerScore = 0;
+round = 0;
+
+document.querySelectorAll('.choice-button').forEach(button => {
+    button.addEventListener("click", () => {
+        let playerInput;
+        switch (button.id) {
+            case "rock-button":
+                playerInput = "rock";
+                break;
+            case "paper-button":
+                playerInput = "paper";
+                break;
+            case "scissors-button":
+                playerInput = "scissors";
+                break;
+        }
+
+        let computerInput = getComputerChoice();
+
+        let score = playRound(playerInput, computerInput);
+        if (score == "win") {
+            humanScore++;
+            alert(`You won! Your current score is ${humanScore}`);
+        } else if (score == "lose") {
+            computerScore++;
+            alert(`You lost. Computer score is currently ${computerScore}`);
+        } else {
+            alert("It's a draw!");
+        }
+
+        div = document.querySelector(".results");
+        const result = document.createElement("p");
+        result.classList.add("result");
+        
+        if (score == "win") {
+            result.textContent = `Player chose ${playerInput} while computer chose ${computerInput}. Player Won!`;
+            result.style.color = "green";
+
+        } else if (score == "lose") {
+            result.textContent = `Player chose ${playerInput} while computer chose ${computerInput}. Player Lost!`;
+            result.style.color = "red";
+        } else {
+            result.textContent = `Player chose ${playerInput} while computer chose ${computerInput}. It's a draw!`;
+            result.style.color = "black";
+        }
+
+        div.appendChild(result);
+        round++;
+
+        if (round == 5) {
+            const gameOver = document.createElement("p");
+            gameOver.classList.add("game-over");
+            gameOver.textContent = `Final Score is Player: ${humanScore} and Computer: ${computerScore}`;
+            gameOver.style.fontWeight = "bold";
+            div.appendChild(gameOver);
+            document.querySelectorAll(".choice-button").forEach(btn => btn.disabled = true);
+            return
+        }
+    });
+});
 
 let playRound = (humanChoice, computerChoice) => {
     if (humanChoice == computerChoice) {
         return "draw";
-    } else if ((humanChoice == "rock" && computerChoice == "scissors") || (humanChoice == "paper" && computerChoice == "rock") || (humanChoice == "scissors" && computerChoice == "rock"))
+    } else if ((humanChoice == "rock" && computerChoice == "scissors") 
+        || (humanChoice == "paper" && computerChoice == "rock") 
+        || (humanChoice == "scissors" && computerChoice == "paper"))
     {
         return "win";
     } else {
         return "lose";
     }
 }
-
-for (let i = 0; i < 5; i++) {
-    let score = playRound(getHumanChoice(),getComputerChoice());
-    if (score == "win") {
-        humanScore++;
-        alert(`You won! Your current score is ${humanScore}`);
-    } else if (score == "lose") {
-        computerScore++;
-        alert(`You lost. Computer score is currently ${computerScore}`);
-    } else {
-        alert("It's a draw!");
-    }
-}
-
-alert(`Final Score is Player: ${humanScore}, Computer: ${computerScore}`);
